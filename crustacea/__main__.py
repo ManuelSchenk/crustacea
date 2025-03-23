0c0239ca988671ec1d97e5d17152a4e931ee9749
Manuel Schenk <manuel.schenk@bosch.de>
2025-03-23 08:35:05 +0100

    add missing content form main

--- crustacea/__main__.py
+++ crustacea/__main__.py
@@ -5,8 +5,9 @@ from crustacea.src.filemenu import FileMenuScreen
 class Crustacea(app.App):
     
     def on_mount(self) -> None:
+        self.file_menu = FileMenuScreen()
         # Start with the menu screen.
-        self.push_screen(FileMenuScreen())
+        self.push_screen(self.file_menu)
 
 
 def main():
